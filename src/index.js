65db162fa0ed2851e2e2805320e2c7356369cebe
floissac <floissac.w@gmail.com>
2018-07-23 21:51:18 +0100

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './libs/fontawesome/fontawesome-all.min';
-import './styles/base.scss';
-import App from './app';
-
-ReactDOM.render(<App />, document.getElementById('root'));
